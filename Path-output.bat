569e48850cd6357559b20e5803e33d4ac06f3473
KentaGoto <gotokentag@gmail.com>
2020-05-27 00:07:17 +0900

    Update Path-output.bat

--- Path-output.bat
+++ Path-output.bat
@@ -1,11 +1,16 @@
 @echo off
 
 setlocal
+
+rem Get the contents of the environment variable "path".
 set p=%path%
+
+rem Outputs environment variables separated by a semicolon, line by line.
 :loop
 FOR /F "tokens=1* delims=;" %%a IN ("%p%") DO (
     echo %%a
     set p=%%b
 )
 if defined p goto :loop
+
 endlocal
